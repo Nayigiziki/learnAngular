2461cba07f2d8b2e7f1d59ef94f2e1e08ab3d5b8
Joseph Nayigiziki <joseph.nayigiziki@gmail.com>
2017-04-14 10:35:30 -0500

    4/14/17 progress

--- src/app/todo/todo-module.js
+++ src/app/todo/todo-module.js
@@ -1,6 +1,11 @@
 angular.module('todo', [])
     .controller('todoController', ['$scope', 'todoModel', function ($scope, model) {
+            $scope.todoCtrl = this;
+            // $scope === this
             // add todo
+            this.submit = function onSubmit() {
+                console.log('value');
+            };
             // delete todo
             // edit todo
     }])
